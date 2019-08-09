7301fd1ceb90ea17e1703b94f8f3aaeaa6370ddd
dong.wang <dong.wang@yingzt.com>
2019-08-09 16:04:55 +0800

    Initial Commit

---
+++ Bmob-Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Bmob-Demo
+//
+//  Created by 王泽龙 on 2019/8/9.
+//  Copyright © 2019 王泽龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
